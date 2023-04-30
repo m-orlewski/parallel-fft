770aa19852f56e4521b0b1131aa1340175f3df65
9orlewski <morlewski@student.agh.edu.pl>
2023-04-30 16:20:26 +0200

    working no parallel loop

--- fft.c
+++ fft.c
@@ -15,6 +15,7 @@ void print(const double complex* data, int size) {
     printf("\n");
 }
 
+/*
 int showInterface(const char* filename) {
     int choice = 0;
     while(!choice) {
@@ -41,6 +42,7 @@ int showInterface(const char* filename) {
             return -1;
     }
 }
+*/
 
 int parse_file(const char* fileName, double complex** data) {
     FILE* file = fopen(fileName, "r");
@@ -116,7 +118,8 @@ int reverse_bits(int N, int n)    //calculating revers number
   return p;
 }
 
-void reorder(complex double* f1, int N) //using the reverse order in the array
+//using the reverse order in the array
+void reorder(complex double* f1, int N)
 {
   complex double f2[200];
   for(int i = 0; i < N; i++)
@@ -125,20 +128,14 @@ void reorder(complex double* f1, int N) //using the reverse order in the array
     f1[j] = f2[j];
 }
 
-void transform(complex double* data, int N)
+void parallelFFT(complex double* data, int N, int rank, int numProcs)
 {
-    //printf("BEFORE REORDER: \n");
-    //print(data, N);
-    reorder(data, N);    //first: reverse order
-    //printf("AFTER REORDER: \n");
-    //print(data, N);
+    reorder(data, N); //first: reverse order
     complex double *W;
     W = (complex double *)malloc(N / 2 * sizeof(complex double));
     W[1] = 1.0 * (cos(-2.0*M_PI/N) + sin(-2.0*M_PI/N)*I);
-    //printf("W[1] = %lf, %lf\n", creal(W[1]), cimag(W[1]));
     W[0] = 1;
     for(int i = 2; i < N / 2; i++) {
-        //W[i] = pow(W[1], i);
         W[i] = W[i-1]*W[1];
     }
     int n = 1;
@@ -150,7 +147,6 @@ void transform(complex double* data, int N)
                 complex double Temp = W[(i * a) % (n * a)] * data[i + n];
                 data[i] = temp + Temp;
                 data[i + n] = temp - Temp;
-                //printf("data[i]=%lf, %lf\n", creal(data[i]), cimag(data[i]));
             }
         }
         n *= 2;
@@ -159,7 +155,8 @@ void transform(complex double* data, int N)
     free(W);
 }
 
-double serial_fft(double complex* data, int N) {
+/*
+double serialFFT(double complex* data, int N) {
     clock_t start, end;
     double cpu_time_used;
     start = clock();
@@ -169,40 +166,55 @@ double serial_fft(double complex* data, int N) {
     end = clock();
     cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-    printf("serial_fft() time taken: %lf\n", cpu_time_used);
+    printf("serialFFT() time taken: %lf\n", cpu_time_used);
     
     write_to_file("output/output.txt", data, N);
     return cpu_time_used;
 }
-
-/*
-void parallel_fft(double complex* data, int N) {
-    Mpi_Init();
-
-    Mpi_Finalize();
-}
 */
 
 int main(int argc, char* argv[]) {
-    //char* filename = malloc(sizeof(char)*200);
+    //char filename[200];
     //int code = showInterface(filename);
     //if(code != 1) return 0;
 
-    double complex* data = NULL;
-    int N = parse_file("data/data.txt", &data);
-    if (N == -1) {
-        printf("parse_file: something went wrong\n");
-        exit(EXIT_FAILURE);
+    int N;
+    double complex* data;
+    int rank, numProcs, nameLen;
+    char   processorName[MPI_MAX_PROCESSOR_NAME];
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);
+    MPI_Get_processor_name(processorName, &nameLen);
+
+    
+    // root process reads data from file and broadcasts
+    if (rank == 0) {
+        N = parse_file("data/data.txt", &data);
+        if (N == -1) {
+            printf("parse_file: something went wrong\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (rank != 0) {
+        data = malloc(N * sizeof(double complex));
+    }
+    MPI_Bcast(data, N, MPI_C_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
+
+    parallelFFT(data, N, rank, numProcs);
+
+    if (rank == 0) {
+        write_to_file("output/output.txt", data, N);
     }
 
-    //free(filename);
-
-    //printf("%d\n", N);
-    //print(data, N);
-    
-    double serial_time = serial_fft(data, N);
-    //double parallel_time = parallel_fft(data, N);
-
     free(data);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (rank == 0)
+        printf("BEFORE FINALIZE(FINALIZE DOESN'T WORK - USE CTRL+C)\n");
+    MPI_Finalize();
+
     return 0;
 }
